9684d7a8780f0285bd4ad7fadb08c6a8fe17d9db
Rohit Wadhwa <rohit.wadhwa52@gmail.com>
2024-03-13 16:54:30 +0530

    Initial commit: Add all project files

---
+++ newtab.js
@@ -0,0 +1,83 @@
+// Example of a basic implementation for randomness
+// This example assumes a maximum of 700 verses across the Bhagavad Gita for simplicity
+const totalChapters = 18;
+const maxVersesPerChapter = [47, 72, 43, 42, 29, 47, 30, 28, 34, 42, 55, 20, 35, 27, 20, 24, 28, 78]; // max verses in each chapter
+
+function getRandomChapter() {
+    return Math.floor(Math.random() * totalChapters) + 1;
+}
+
+function getRandomVerse(chapter) {
+    return Math.floor(Math.random() * maxVersesPerChapter[chapter - 1]) + 1;
+}
+
+// Fetch and display the quote
+function fetchQuote() {
+    const quoteElement = document.getElementById('quote');
+    const reloadButton = document.getElementById('reload');
+
+    // Disable the button and show loading state
+    reloadButton.disabled = true;
+    reloadButton.textContent = 'Loading...';
+    const chapter = getRandomChapter();
+    const verse = getRandomVerse(chapter);
+    fetch(`https://bhagavadgitaapi.in/slok/${chapter}/${verse}/`)
+        .then(response => response.json())
+        .then(data => {
+            quoteElement.innerHTML = `
+      <p>${data.slok}</p>
+      <p>${data.tej.ht}</p>`;
+            quoteElement.style.opacity = 0; // Prepare for animation
+            setTimeout(() => {
+                quoteElement.style.opacity = 1; // Start animation
+            }, 1200); // Short delay to ensure the opacity change triggers animation
+            reloadButton.disabled = false;
+            reloadButton.textContent = '⟳ Reload';
+        })
+        .catch(error => {
+            console.error('Error fetching the quote:', error);
+            // Handle error state here, optionally re-enabling the button or providing user feedback
+            reloadButton.disabled = false;
+            reloadButton.textContent = '⟳ Reload';
+        });
+
+}
+
+// Reload quote
+document.getElementById('reload').addEventListener('click', function() {
+    // const quoteElement = document.getElementById('quote');
+    // quoteElement.innerHTML =`Loading...`;
+        fetchQuote();
+        updateDateTime();
+});
+
+// Update date and time
+// Generate and update date-time with animation
+function updateDateTime() {
+    const now = new Date();
+    const dateString = now.toLocaleDateString(undefined, { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+    const timeString = now.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit', second: '2-digit'});
+
+    // Update date
+    const dateTimeContainer = document.getElementById('date-time');
+    dateTimeContainer.innerHTML = `<div>${dateString}</div><div class="time">${timeString}</div>`;
+}
+
+// Initialize
+fetchQuote();
+updateDateTime();
+// Update the date and time every minute
+setInterval(updateDateTime, 1000);
+
+document.getElementById('music-toggle').addEventListener('click', function() {
+    const music = document.getElementById('background-music');
+    if (music.paused) {
+        music.play();
+        this.textContent = '🔇 Stop Music'; // Update button text
+    } else {
+        music.pause();
+        this.textContent = '🔊 Play Music'; // Update button text
+    }
+});
+
+// Initialize the page with your existing functions
